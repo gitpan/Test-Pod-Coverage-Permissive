fc948e546e94fe9e9019b0419c99d5465e1a1da9
Андрій Костенко <andrey@kostenko.name>
2010-02-04 15:49:08 +0000

    Import of GUGU/Test-Pod-Coverage-Permissive-0.02 from CPAN.
    gitpan-cpan-distribution: Test-Pod-Coverage-Permissive
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         GUGU/Test-Pod-Coverage-Permissive-0.02.tar.gz
    gitpan-cpan-author:       GUGU
    gitpan-cpan-maturity:     released

--- lib/Test/Pod/Coverage/Permissive.pm
+++ lib/Test/Pod/Coverage/Permissive.pm
@@ -28,11 +28,11 @@ Test::Pod::Coverage::Permissive - Checks for pod coverage regression.
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
